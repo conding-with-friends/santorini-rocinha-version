0d117fb230469394d140c3a095d9c109a4ddf52f
Rafael Motta de Carvalho <rafael.carvalho@volders.de>
2020-04-12 16:05:57 -0300

    Sketch of Redux approach

--- santorini/src/Game2.js
+++ santorini/src/Game2.js
@@ -1,44 +1,78 @@
+const invalidSetupPositionError = new Error('Position invalid for setup phase')
 const invalidPositionError = new Error('Position out of boundaries')
 const notVacantPositionError = new Error('Position already taken')
-const notOnTurnError = new Error('Not on turn')
+const notOnPhaseError = new Error('Not on phase')
+const invalidMoveToLevelError = new Error('Invalid move to level')
+const invalidRangeError = new Error('Invalid range for move or build')
 
 // Dummy state
 // {
 //   playersCount: 2,
 //   currentPlayer: 0, // 0, 1
-//   currentHero: 0, // null, 0, 1
-//   turn: 'setup', // setup, move-and-build
+//   phase: 'setup', // setup, move-and-build
 //   board: {
-//     '0,1': 0
+//     '0,0': 0, '1,0': 0, '2,0': 0, '3,0': 0, '4,0': 0,
+//     '0,1': 0, '1,1': 0, '2,1': 0, '3,1': 0, '4,1': 0,
+//     '0,2': 0, '1,2': 0, '2,2': 0, '3,2': 0, '4,2': 0,
+//     '0,3': 0, '1,3': 0, '2,3': 0, '3,3': 0, '4,3': 0,
+//     '0,4': 0, '1,4': 0, '2,4': 0, '3,4': 0, '4,4': 0
 //   },
 //   heroes: {
-//     '0,0': null,
-//     '0,1': null,
-//     '1,0': null,
-//     '1,1': '0,0'
+//     '0,0': null, '0,1': null,
+//     '1,0': null, '1,1': null
 //   }
 // }
 
 function santorini (action, options, state) {
   switch (action) {
-    case 'SETUP':
-      const { currentPlayer, playersCount, heroes, turn, board } = state
+    case 'SETUP': // ['0,0', '0,1']
+      const { currentPlayer, playersCount, heroes, phase, board } = state
 
-      validateTurn('setup', turn)
-      validatePositions(...options, board)
-      validateVacantPositions(...options, heroes)
+      validatePhase('setup', phase)
+      validateSetupPositions(...options, board)
+      validateBoardPosition(options[0], board)
+      validateBoardPosition(options[1], board)
+      validateVacantPosition(options[0], heroes)
+      validateVacantPosition(options[1], heroes)
 
       return {
         ...state,
         currentPlayer: getNextPlayer(currentPlayer, playersCount),
-        turn: getNextTurn(currentPlayer, heroes),
+        phase: getNextPhase(currentPlayer, heroes),
         heroes: {
           ...heroes,
           [`${currentPlayer},0`]: options[0],
           [`${currentPlayer},1`]: options[1]
         }
       }
-    case 'MOVE_AND_BUILD'
+    case 'MOVE_AND_BUILD': // { move: '1,1', build: '2,2', hero: 0 }
+      const { currentPlayer, playersCount, heroes, phase, board } = state
+      const { move, build, hero } = options
+
+      validatePhase('move-and-build', phase)
+      validateBoardPosition(move, board)
+      validateBoardPosition(build, board)
+      validateVacantPosition(move, heroes)
+      validateVacantPosition(build, heroes)
+
+      const heroPosition = heroes[`${currentPlayer},${hero}`]
+      validateRange(heroPosition, move)
+      validateRange(move, build)
+      validateMoveToLevel(heroPosition, move, board)
+
+      return {
+        ...state,
+        currentPlayer: getNextPlayer(currentPlayer, playersCount),
+        board: {
+          ...board,
+          [build]: board[build] + 1
+        },
+        heroes: {
+          ...heroes,
+          [`${currentPlayer},${hero}`]: move
+        }
+      }
+    }
     default:
   }
 }
@@ -49,7 +83,7 @@ function getNextPlayer (currentPlayer, playersCount) {
   return nextPlayer === playersCount ? 0 : nextPlayer
 }
 
-function getNextTurn (currentPlayer, heroes) {
+function getNextPhase (currentPlayer, heroes) {
   const {
     [`${currentPlayer},0`]: _,
     [`${currentPlayer},1`]: __,
@@ -61,21 +95,33 @@ function getNextTurn (currentPlayer, heroes) {
     : 'setup'
 }
 
-function validateTurn(expectedTurn, currentTurn) {
-  if (expectedTurn !== currentTurn) throw notOnTurnError
+function validatePhase (expectedPhase, currentPhase) {
+  if (expectedPhase !== currentPhase) throw notOnPhaseError
 }
 
-function validatePositions(position0, position1, board) {
-  if (!position0 || !position1) throw invalidPositionError
-
-  const positions = Object.keys(board)
-
-  if (!positions.includes(position0) || !positions.includes(position1)) {
-    throw invalidPositionError
-  }
+function validateSetupPositions (position0, position1, board) {
+  if (!position0 || !position1) throw invalidSetupPositionError
+  if (position0 === position1) throw invalidSetupPositionError
 }
 
-function validateVacantPositions(position0, position1, heroes) {
-  if (position0 === position1) throw notVacantPositionError
-  if (heroes[position0] || heroes[position1]) throw notVacantPositionError
+function validateBoardPosition (position, board) {
+  if (!Object.keys(board).includes(position)) throw invalidPositionError
+}
+
+function validateVacantPosition (position, heroes) {
+  if (heroes[position]) throw notVacantPositionError
+}
+
+function validateMoveToLevel (currentPosition, move, board) {
+  if (board[currentPosition] + 1 < board[move]) throw invalidMoveToLevelError
+}
+
+function validateRange (origin, destination) {
+  if (origin === destination) throw invalidRangeError
+
+  const originX, originY = origin.split(',')
+  const destinationX, destinationY = destination.split(',')
+
+  if (Math.abs(parseInt(originX) - parseInt(destinationX)) > 1) throw invalidRangeError
+  if (Math.abs(parseInt(originY) - parseInt(destinationY)) > 1) throw invalidRangeError
 }
