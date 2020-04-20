635272650d89bf1897af0a1ce807ff78e703fe32
Max Salles <herond12@gmail.com>
2020-04-19 21:27:53 -0300

    Fixed and refactored santorini specs

--- santorini/src/__tests__/santorini.js
+++ santorini/src/__tests__/santorini.js
@@ -1,6 +1,5 @@
 import santorini, { initialState } from '../santorini'
 import {
-
   invalidSetupPositionError,
   invalidPositionError,
   notVacantPositionError,
@@ -78,15 +77,15 @@ describe('santorini', () => {
     it('sets new player`s position and build a level', () => {
       const options = { moveTo: '1,2', buildAt: '1,3', hero: 0 }
 
-      expect(santorini(action, options)).toEqual({
-        ...initialState,
-        currentPlayer: 0,
+      expect(santorini(action, options, currentState)).toEqual({
+        ...currentState,
+        currentPlayer: 1,
         heroes: {
-          ...initialState.heroes,
+          ...currentState.heroes,
           '0,0': '1,2'
         },
         board: {
-          ...initialState.board,
+          ...currentState.board,
           '1,3': 1
         }
       })
@@ -106,37 +105,41 @@ describe('santorini', () => {
     })
 
     it('throws invalidPositionError if no move position is given', () => {
-      const options = [{ buildAt: '2,2', hero: 0 }]
+      const options = { buildAt: '2,2', hero: 0 }
 
-      expect(() => santorini(action, options, initialState)).toThrow(invalidPositionError)
+      expect(() => santorini(action, options, currentState)).toThrow(invalidPositionError)
     })
 
     it('throws invalidPositionError when no build position is given', () => {
-      const options = [{ moveTo: '2,2', hero: 0 }]
+      const options = { moveTo: '2,2', hero: 0 }
 
-      expect(() => santorini(action, options, initialState)).toThrow(invalidPositionError)
+      expect(() => santorini(action, options, currentState)).toThrow(invalidPositionError)
     })
 
     it('throws invalidPositionError if trying to move out of board`s boundaries', () => {
-      const options = [{ moveTo: '10,10', buildAt: '2,2', hero: 0 }]
+      const options = { moveTo: '10,10', buildAt: '2,2', hero: 0 }
 
-      expect(() => santorini(action, options, initialState)).toThrow(invalidPositionError)
+      expect(() => santorini(action, options, currentState)).toThrow(invalidPositionError)
     })
 
     it('throws invalidPositionError when trying to build out of board`s boundaries', () => {
-      const options = [{ moveTo: '1,1', buildAt: '20,20', hero: 0 }]
+      const options = { moveTo: '1,1', buildAt: '20,20', hero: 0 }
 
-      expect(() => santorini(action, options, initialState)).toThrow(invalidPositionError)
+      expect(() => santorini(action, options, currentState)).toThrow(invalidPositionError)
     })
 
-    it('throws notVacantPositionError when move position is already taken', () => {
-      const options = [{ moveTo: '2,2', buildAt: '2,3', hero: 0 }]
+    it('throws "notVacantPositionError" when move position is already taken', () => {
+      const options = { moveTo: '2,2', buildAt: '2,3', hero: 0 }
       const currentState = {
         ...initialState,
+        phase: 'MOVE_AND_BUILD',
         currentPlayer: 0,
         heroes: {
           ...initialState.heroes,
-          '1,1': '2,2'
+          '0,0': '1,1',
+          '0,1': '2,2',
+          '1,0': '3,3',
+          '1,1': '4,4'
         }
       }
 
@@ -144,80 +147,128 @@ describe('santorini', () => {
     })
 
     it('throws notVacantPositionError when build position is already taken', () => {
-      const options = [{ moveTo: '1,1', buildAt: '2,2', hero: 0 }]
+      const options = { moveTo: '1,2', buildAt: '2,2', hero: 0 }
       const currentState = {
         ...initialState,
+        phase: 'MOVE_AND_BUILD',
         currentPlayer: 0,
         heroes: {
           ...initialState.heroes,
-          '1,1': '2,2'
+          '0,0': '1,1',
+          '0,1': '2,2',
+          '1,0': '3,3',
+          '1,1': '4,4'
         }
       }
 
       expect(() => santorini(action, options, currentState)).toThrow(notVacantPositionError)
     })
 
-    it('throws invalidRangeError when move position is out of range', () => {
-      const options = [{ moveTo: '3,3', buildAt: '4,4', hero: 0 }]
+    it('throws "invalidRangeError" when move position is out of range', () => {
+      const options = { moveTo: '3,4', buildAt: '2,4', hero: 0 }
       const currentState = {
         ...initialState,
+        phase: 'MOVE_AND_BUILD',
         currentPlayer: 0,
         heroes: {
           ...initialState.heroes,
-          '0,1': '1,1'
+          '0,0': '1,1',
+          '0,1': '2,2',
+          '1,0': '3,3',
+          '1,1': '4,4'
         }
       }
 
       expect(() => santorini(action, options, currentState)).toThrow(invalidRangeError)
     })
 
-    it('throws invalidRangeError when build position is out of range', () => {
-      const options = [{ moveTo: '2,2', buildAt: '4,4', hero: 0 }]
+    it('throws "invalidRangeError" when build position is out of range', () => {
+      const options = { moveTo: '0,0', buildAt: '2,4', hero: 0 }
       const currentState = {
         ...initialState,
+        phase: 'MOVE_AND_BUILD',
         currentPlayer: 0,
         heroes: {
           ...initialState.heroes,
-          '0,1': '1,1'
+          '0,0': '1,1',
+          '0,1': '2,2',
+          '1,0': '3,3',
+          '1,1': '4,4'
         }
       }
 
       expect(() => santorini(action, options, currentState)).toThrow(invalidRangeError)
     })
 
-    it('throws invalidMoveToLevelError when trying to step more than 1 level on a move', () => {
-      const options = [{ moveTo: '3,3', buildAt: '4,4', hero: 0 }]
+    it('throws "invalidMoveToLevelError" when trying to step more than 1 level on a move', () => {
+      const options = { moveTo: '2,1', buildAt: '3,1', hero: 0 }
       const currentState = {
         ...initialState,
+        phase: 'MOVE_AND_BUILD',
         currentPlayer: 0,
         heroes: {
           ...initialState.heroes,
-          '0,1': '2,2'
+          '0,0': '1,1',
+          '0,1': '2,2',
+          '1,0': '3,3',
+          '1,1': '4,4'
         },
         board: {
           ...initialState.board,
-          '2,2': 0
-          '3,3': 2
+          '1,1': 0,
+          '2,1': 2
         }
       }
 
-      expect(() => santorini(action, options, currentState)).toThrow(invalidRangeError)
+      expect(() => santorini(action, options, currentState)).toThrow(invalidMoveToLevelError)
     })
 
     describe('when a slot is on the highest level', () => {
-      const currentState = {
-        ...initialState,
-        currentPlayer: 0,
-        board: {
-          ...initialState.board,
-          '1,3': 4
+      it('throws "invalidMoveToLevelError" when trying to move on it', () => {
+        const options = { moveTo: '1,2', buildAt: '2,3', hero: 0 }
+        const currentState = {
+          ...initialState,
+          phase: 'MOVE_AND_BUILD',
+          currentPlayer: 0,
+          heroes: {
+            ...initialState.heroes,
+            '0,0': '1,1',
+            '0,1': '2,2',
+            '1,0': '3,3',
+            '1,1': '4,4'
+          },
+          board: {
+            ...initialState.board,
+            '1,1': 3,
+            '1,2': 4
+          }
         }
-      }
 
-      it('throws invalidBuildLevelError when trying to build on it', () => {
-        const options = [{ moveTo: '1,2', buildAt: '1,3', hero: 0 }]
+        expect(() => santorini(action, options, currentState)).toThrow(invalidMoveToLevelError)
+      })
 
-        expect(() => santorini(action, options, currentState)).toThrow(invalidRangeError)
+      it('throws "invalidBuildLevelError" when trying to build on it', () => {
+        const options = { moveTo: '1,2', buildAt: '1,3', hero: 0 }
+        const currentState = {
+          ...initialState,
+          phase: 'MOVE_AND_BUILD',
+          currentPlayer: 0,
+          heroes: {
+            ...initialState.heroes,
+            '0,0': '1,1',
+            '0,1': '2,2',
+            '1,0': '3,3',
+            '1,1': '4,4'
+          },
+          board: {
+            ...initialState.board,
+            '1,1': 2,
+            '1,2': 3,
+            '1,3': 4
+          }
+        }
+
+        expect(() => santorini(action, options, currentState)).toThrow(invalidBuildLevelError)
       })
     })
   })
